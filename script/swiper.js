57a326613011f7398dcf0eb410a3047e0e70b746
Dahee <57856201+Dev-hee@users.noreply.github.com>
2024-07-30 11:16:44 +0900

    Add files via upload

--- script/swiper.js
+++ script/swiper.js
@@ -8,3 +8,14 @@ var mySwiper = new Swiper('.swiper-container', {
     prevEl: ".swiper-button-prev",
   },
 });
+
+var swiper = new Swiper(".mySwiper", {
+  
+  slidesPerView:6,
+  slidesPerGroup:2,
+  autoplay:{delay:2000},
+  scrollbar: {
+  loop:true,
+  el: ".swiper-scrollbar",
+  },
+});
